9157b063232dd81dd7a43319fea7e89e0d259e1a
Willian <willianedi@hotmail.com>
2023-05-22 17:06:11 -0300

    initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from "@phosphor-icons/react";
+import { Avatar } from "./Avatar";
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.Sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/WillianBorges1997.png" />
+
+        <strong>Willian Borges</strong>
+        <span>Trainee</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
